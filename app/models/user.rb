575d58809db020f83bc2095de275c7cebc6b275e
Stas Anikin <stas.terrace@gmail.com>
2021-02-15 17:23:53 -0800

    created welcome controller, navbar, session views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
   has_secure_password
 
-  validates_presence_of :email
-  validates_uniqueness_of :email
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
+  def full_name
+    "#{first_name} #{last_name}".titleize
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get("/home", to: "welcome#home")
+  get("/about", to: "welcome#about")
+  get("/", { to: "welcome#home", as: :root })
+  resource :session, only: [:new, :create, :destroy]
+  resources :users
 end
