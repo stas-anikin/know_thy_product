7268735261d31a9ed7e2322dd669746d3afe2bbe
Stas Anikin <stas.terrace@gmail.com>
2021-02-18 20:37:27 -0800

    added bootstrap theme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,16 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require material/bootstrap-material-design
+//= require material/perfect-scrollbar.jquery.min
+//= require material/chartist.min
+//= require material/arrive.min
+//= require material/bootstrap-notify
+//= require material/material-dashboard
+//= require material/demo
 
 Rails.start()
 ActiveStorage.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get "page/index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get("/home", to: "welcome#home")
   get("/about", to: "welcome#about")
   get("/", { to: "welcome#home", as: :root })
   get("/admin/panel", to: "welcome#admin_panel")
+  get("/dashboard", to: "page#index")
 
   resource :session, only: [:new, :create, :destroy]
   resources :departments
