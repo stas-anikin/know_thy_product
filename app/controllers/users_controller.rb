87e8c4fc90bf782402cccab8d92f4f6b184a116d
Stas Anikin <stas.terrace@gmail.com>
2021-02-19 13:57:10 -0800

    added role selection to user sign up and styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :role_id)
   end
 end
